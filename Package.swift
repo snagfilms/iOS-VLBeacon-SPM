10983100d1ec363cac8c9d36c494e26f819d3140
rakesh-vl <rakeshsharma@viewlift.com>
2026-07-23 15:14:02 +0530

    revert to dynamic

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "VLBeaconLib",
-            type: .static,
+            type: .dynamic,
             targets: ["VLBeaconLib"]),
     ],
     targets: [
